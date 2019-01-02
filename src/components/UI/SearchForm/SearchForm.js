49f80434858124e3207f2ba00bc3b700e1a79700
Marcin Chojnacki <marcinchojnacki00@gmail.com>
2019-01-02 22:44:30 +0100

    work with search form

--- src/components/UI/SearchForm/SearchForm.js
+++ src/components/UI/SearchForm/SearchForm.js
@@ -1,25 +1,85 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { DateRange  } from 'react-date-range';
+import { format, addDays } from 'date-fns';
+import { Container, Form } from 'semantic-ui-react';
 
-import DatePicker from '../DatePicker/DatePicker';
-import { Container, Form } from 'semantic-ui-react'
+import classes from './SearchForm.css'
 
-const searchForm = (props) => {
-
-  const datePickerHandler = () => {
-    console.log('test2');
-  };
-
-  return (
-    <Container>
-      <Form onSubmit={props.submit}>
-        <Form.Group style={{justifyContent: 'center'}}>
-          <Form.Input width={6} loading name='searchValue' value={props.searchValue} onChange={props.searchChange} placeholder='Search...' />
-          <DatePicker />
-          <Form.Button type="submit" content='Szukaj' primary />
-        </Form.Group>
-      </Form>
-    </Container>
-  );
+function formatDateDisplay(date, defaultText) {
+  if (!date) return defaultText;
+  return format(date, 'DD-MM-YYYY');
 }
 
-export default searchForm;
+class SearchForm extends Component {
+
+	state = {
+		dateRangePicker: {
+			selection: {
+				startDate: new Date(),
+				endDate: addDays(new Date(), 7),
+				key: 'selection',
+			},
+		},
+		visible: false
+	}
+
+	toggleVisibility = () => this.setState({ visible: !this.state.visible });
+
+	toggleOveraly = () => this.setState({visible: false});
+
+	handleRangeChange(which, payload) {
+    // console.log('which', which, 'payload', payload);
+    this.setState({
+      [which]: {
+        ...this.state[which],
+        ...payload,
+      },
+    });
+  }
+
+	render() {
+		const { visible } = this.state;
+
+		return (
+			<div className={classes.SearchFormContainer}>
+        <div className={visible ? classes.Overlay : null} onClick={this.toggleOveraly}></div>
+        <Container>
+          <Form onSubmit={this.props.submit}>
+            <Form.Group style={{justifyContent: 'center'}}>
+              <Form.Input width={6} loading name='searchValue' value={this.props.searchValue} onChange={this.props.searchChange} placeholder='Search...' />
+
+              <Form.Input 
+              className={classes.ExtraMargin}
+              type="text"
+              name="startDate"
+              readOnly
+              onClick={this.toggleVisibility}
+              value={formatDateDisplay(this.state.dateRangePicker.selection.startDate)}
+              />
+              <Form.Input 
+              type="text"
+              name="endDate"
+              readOnly
+              onClick={this.toggleVisibility}
+              value={formatDateDisplay(this.state.dateRangePicker.selection.endDate)}
+              />
+              <Form.Button type="submit" content='Szukaj' primary />
+            </Form.Group>
+          </Form>
+        </Container>
+			  <div className={visible ? [classes.DateBox, classes.open].join(' ') : classes.DateBox}>
+						<DateRange
+              onChange={(e) => this.handleRangeChange('dateRangePicker', e)}
+							showSelectionPreview={false}
+							moveRangeOnFirstSelection={false}
+							months={2}
+							ranges={[this.state.dateRangePicker.selection]}
+							direction="horizontal"
+						/>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default SearchForm;
--- src/containers/EventFinder/EventFinder.js
+++ src/containers/EventFinder/EventFinder.js
@@ -6,28 +6,41 @@ class EventFinder extends Component {
 
   state = {
     searchValue: '',
-    isLoading: false
+    startDate: '',
+    testowy: '',
+    isLoading: false,
+    artist: 'Maroon 5',
+    data: null
   }
 
   inputValueHandler = (e) => {
     this.setState({[e.target.name]: e.target.value});
+    console.log('target', e.target.value);
+  }
+
+  test = (e) => {
     console.log(e.target.value);
   }
 
   formSubmitHandler = (e) => {
     e.preventDefault();
-    // this.setState({value: e.target.value});
-    // console.log(e.target.value);
-    console.log('test');
+    console.log(new URLSearchParams(new FormData(e.target)).toString());
+    fetch(`https://rest.bandsintown.com/artists/${this.state.searchValue}/events?app_id=8cd32220-ea94-4c7a-a074-ec271e841187&date=2019-01-01%2C2019-06-20`)
+    .then(response => response.json())
+    .then(json => this.setState({data: json}))
+    .catch(error => console.log(error));
+    // console.log('test1');
   }
 
   render() {
+    console.log(this.state.data);
     return (
       <div>
         <SearchForm 
           column={8}
           searchValue={this.state.searchValue}
           searchChange={this.inputValueHandler}
+          testowy={this.test}
           isLoading={this.state.isLoading}
           submit={this.formSubmitHandler} />
       </div>
