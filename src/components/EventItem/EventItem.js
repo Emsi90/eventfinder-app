4d5b7366c8560b83e7e40294dde4a7305335f10c
Marcin Chojnacki <marcinchojnacki00@gmail.com>
2019-01-14 23:58:56 +0100

    work with layout

--- src/components/EventItem/EventItem.js
+++ src/components/EventItem/EventItem.js
@@ -1,16 +1,36 @@
 import React from 'react';
 
+import { Icon } from 'semantic-ui-react'
 import { format } from 'date-fns';
 import classses from './EventItem.css';
 
-const eventItem = ({ artist, dateTime, city, country }) => {
+const eventItem = ({ artist, artistImg, artistUrl, artistFb, dateTime, city, country, place, latitude, longitude, ticketsStatus, ticketsUrl, venueUrl }) => {
   return (
     <li className={classses.eventItem}>
 
-      <p>{artist}</p>
-      <p>{format(dateTime, 'HH:MM DD-MM-YYYY')}</p>
-      <p>{city}</p>
-      <p>{country}</p>
+      <div className={classses.BoxLeft}>
+        <img src={artistImg} alt={artist}/>
+        <div className={classses.artistInfo}>
+          <h2 className={classses.artistHeader}>{artist}</h2>
+          <div className={classses.media}>
+            <a href={artistUrl} className={classses.link} target="_blank"><Icon disabled name='globe' />www</a>
+            <a href={artistFb} className={classses.link} target="_blank"><Icon disabled name='facebook' />Facebook</a>
+          </div>
+        </div>
+      </div>
+      <div className={classses.BoxRight}>
+        <span className={classses.date}>{format(dateTime, 'DD-MM-YYYY')} / <span className={classses.bold}>{format(dateTime, 'HH:MM')}</span></span>
+        <span className={classses.city}>{city} / {country}</span>
+        <span className={classses.place}>{place}</span>
+        <div className={classses.media}>
+          <a href={`http://maps.google.com/maps?q=${latitude},${longitude}`} className={classses.link} target="_blank"><Icon disabled name='map marker' />view on Map</a>
+          <a href={venueUrl} className={classses.link} target="_blank"><Icon disabled name='globe' />event site</a>
+        </div>
+        <div className={classses.tickets}>
+          <span className={ticketsStatus === 'available' ? classses.statusOk : classses.stausNo}>{ticketsStatus}</span>
+          {ticketsStatus === 'available' ? <a href={ticketsUrl} className="ui positive button" target="_blank">Buy Ticket</a> : <a href={ticketsUrl} className="ui negative button" disabled>No Tickets</a>}
+        </div>
+      </div>
     </li>
   );
 };
--- src/components/EventsList/EventsList.js
+++ src/components/EventsList/EventsList.js
@@ -9,20 +9,43 @@ const eventsList = (props) => {
   console.log(props.artistData);
   console.log(props.data);
 
-  const data = props.data;
+  const {data, artistData} = props;
 
   const eventItem = data => {
     const artist = data.lineup[0];
+    const artistImg = artistData.thumb_url;
+    const artistUrl = artistData.url;
+    const artistFb = artistData.facebook_page_url;
     const dateTime = data.datetime;
     const city = data.venue.city;
     const country = data.venue.country;
-    return <EventItem key={data.id} artist={artist} dateTime={dateTime} city={city} country={country} />
+    const place = data.venue.name;
+    const latitude = data.venue.latitude;
+    const longitude = data.venue.longitude;
+    const ticketsStatus = data.offers[0].status;
+    const ticketsUrl = data.offers[0].url;
+    const venueUrl = data.url;
+    return <EventItem
+      key={data.id}
+      artist={artist}
+      artistImg = {artistImg}
+      artistUrl = {artistUrl}
+      artistFb = {artistFb}
+      dateTime={dateTime}
+      city={city}
+      country={country}
+      place = {place}
+      latitude = {latitude}
+      longitude = {longitude}
+      ticketsStatus = {ticketsStatus}
+      ticketsUrl = {ticketsUrl}
+      venueUrl = {venueUrl} />
   }
 
   return(
     <Container>
       <ul className={classes.eventList}>
-        {data !== null ? data.map(eventItem) : 'Please search artist...'}
+        {data !== null && artistData !== null ? data.map(eventItem) : 'Please search artist...'}
       </ul>
     </Container>
   );
